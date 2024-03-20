b5ee042f3748c65b77786f882b3b9329e9ca1eeb
mindexpert7546 <Kundan.Kumar@iinterchange.net>
2024-03-20 12:30:43 +0530

    New sheet created in the mypoi.xlsx

--- src/main/java/com/iic/PoiDemo.java
+++ src/main/java/com/iic/PoiDemo.java
@@ -1,5 +1,6 @@
 package com.iic;
 
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -10,9 +11,12 @@ import java.io.IOException;
 
 public class PoiDemo {
     public static void main(String[] args) throws IOException {
-        String filePath = "C:\\Users\\Kundan.kumar\\IdeaProjects\\Apache_POI_\\mypoi.xlsx";
-        File myFile = new File(filePath);
-        Workbook workbook = new XSSFWorkbook();
+        String filePath = "C:\\Users\\Kundan.kumar\\Documents\\Apache_POI_\\OutPut\\mypoi.xlsx"; // File path
+        File myFile = new File(filePath); //To create the file
+
+        Workbook workbook = new XSSFWorkbook(); // code for create the excel file
+        Sheet sheet1 = workbook.createSheet("sheet1"); // create the new sheet in mypoi.xlsx
+
         FileOutputStream fileOutputStream = new FileOutputStream(myFile);
         workbook.write(fileOutputStream);
         fileOutputStream.close();
